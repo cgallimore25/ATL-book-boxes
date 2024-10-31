6f3bbf5d8263551ec525b221e4870c0593dd91d0
Connor Gallimore <cgallimore1@gsu.edu>
2024-10-31 19:59:17 -0400

    Initial commit, got modules working, contains reactive palette instantiation with button server nested

---
+++ modules/tabMapServer.R
@@ -0,0 +1,71 @@
+## Server module for managing the interactive map
+
+tabMapServer <- function(id, n_bins, var_lookup, zip_df, sub_z_srt, merged_dat) {
+  moduleServer(id, function(input, output, session) {
+    
+    # Initial reactive palettes for map elements
+    palettes <- reactiveValues(
+      selected_palette = "cividis", 
+      zip_pal = "Reds",     
+      box_pal = viridis::viridis(n_bins, option = "cividis")
+    )
+    
+    # Manage color palette buttons with the paletteButtonServer module
+    paletteButtonServer("palette_buttons", palettes, num_bins = n_bins)
+    
+    # Create base map
+    output$map <- renderLeaflet({
+      leaflet() %>%
+        addTiles() %>%
+        setView(lng = -84.4, lat = 33.75, zoom = 10) %>%
+        addMiniMap(tiles = providers$Esri.WorldStreetMap, position = "bottomleft", toggleDisplay = TRUE)
+    })
+    
+    # Manage zip borders overlay------------------------------------------------
+    observe({
+      if (input$show_zip_brds) {
+        color_zip_by <- input$color_zip_by
+        zip_cdata <- zip_df[[color_zip_by]]
+        pal <- colorBin(palettes$zip_pal, domain = zip_cdata, bins = n_bins, pretty = FALSE)
+        
+        h_opts <- highlightOptions(color = "black", weight = 2, bringToFront = FALSE, fillOpacity = 0.9)
+        
+        leafletProxy("map", data = sub_z_srt) %>%
+          clearGroup("zip_borders") %>%
+          removeControl("bords_legend") %>%
+          addPolygons(fillColor = ~pal(zip_cdata), fillOpacity = 0.7, color = "black", weight = 1, group = "zip_borders",
+                      highlightOptions = h_opts,
+                      popup = ~paste("Zip:", ZCTA5CE10, "<br>", var_lookup[[color_zip_by]], ":", zip_cdata)) %>%
+          addLegend("topright", pal = pal, values = zip_cdata, title = var_lookup[[color_zip_by]], layerId = "bords_legend", labFormat = labelFormat(digits = 1))
+      } else {
+        leafletProxy("map") %>%
+          clearGroup("zip_borders") %>%
+          removeControl("bords_legend")
+      }
+    })
+    
+    # Manage book box markers overlay-------------------------------------------
+    observe({
+      if (input$show_box_locs) {
+        color_box_by <- input$color_box_by
+        box_cdata <- merged_dat[[color_box_by]]
+        pal_pts <- if (color_box_by %in% c("self_c", "charted")) colorFactor(palettes$box_pal, box_cdata) else colorBin(palettes$box_pal, domain = box_cdata, pretty = FALSE)
+        
+        zoom_level <- input$map_zoom
+        radius <- 110000 * (0.6 ^ zoom_level)
+        
+        leafletProxy("map", data = merged_dat) %>%
+          clearGroup("book_boxes") %>%
+          removeControl("boxes_legend") %>%
+          addCircles(lng = ~Longitude, lat = ~Latitude, radius = radius, color = ~pal_pts(box_cdata),
+                     stroke = FALSE, fillOpacity = 0.8, group = "book_boxes",
+                     popup = ~paste("Latitude:", Latitude, "<br>", "Longitude:", Longitude, "<br>", "Address:", paste0(Number, " ", Street, ", ", Zip), "<br>", var_lookup[[color_box_by]], ":", box_cdata)) %>%
+          addLegend("bottomright", pal = pal_pts, values = box_cdata, title = var_lookup[[color_box_by]], layerId = "boxes_legend", labFormat = labelFormat(digits = 1))
+      } else {
+        leafletProxy("map") %>%
+          clearGroup("book_boxes") %>%
+          removeControl("boxes_legend")
+      }
+    })
+  })
+}
