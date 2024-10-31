8274ddab4d62f1fe9a358cb8c8951724b628429b
Connor Gallimore <cgallimore1@gsu.edu>
2024-10-31 01:54:15 -0400

    Filled-in 'About' tab

--- book_box_density_app_rm_mod.R
+++ book_box_density_app_rm_mod.R
@@ -82,17 +82,28 @@ ui <- navbarPage("100+ ATL Book Boxes", id = "nav",
    
    tabPanel("About",
             fluidPage(
-              h2("About This Project"),
-              p("This interactive map visualizes the density of book boxes across various zip codes. The data presented includes demographic information, community resources, and socioeconomic indicators."),
+              h4("Inspiration"),
+              p("This project was inspired by my observation that many of the public bookcases in my area were not listed on the Little Free Library web map.
+                 From 2023-2024, I recorded longitude and latitude coordinates for the ones I discovered, and cross-referenced them with the listings, culminating in this dataset of 105 bookboxes in the Greater Atlanta area."),
               
-              p("Data Sources:"),
+              h4("The Child Opportunity Index"),
+              p("Being interested in indicators and issues of health, education, safety, and socioeconomic inequity, I incorporated nationally normalized zip code estimates from the Child Opportunity Index (COI; link).
+                 The COI is a composite index of children's neighborhood opportunity, consisting of 3 major domains: Education (ED), Health and Environment (HE), and Social and Economic factors (SE)."),
+              
+              h4("Explanation of variables:"),
               tags$ul(
                 tags$li("Book Box Locations: Collected from community engagement."),
                 tags$li("Community of Interest (COI) Data: Provided by relevant social services."),
                 tags$li("Demographic Data: Sourced from the U.S. Census Bureau.")
               ),
               
-              p("For further details, please refer to the project's documentation or contact the project team."),
+              h4("Usage"),
+              p("Hovering over plotted zip codes highlights their borders. 
+                 Clicking any border shows the zip code and value of the color variable. 
+                 Clicking book box locations shows their coordinates, address, and value of the color variable.
+                 Border and box location colors are disentangled, allowing visualization of different COI scores alongside the distribution of boxes.
+                 Toggle different color schemes by clicking the palettes. Happy hunting, bookworms :)"),
+              
               hr(),
               h4("Additional Information"),
               p("This project aims to foster community awareness and promote resource sharing through an engaging and informative interface.")
