7a24ba12f40a4fd297514ac977e77c3caf2c51be
Connor Gallimore <cgallimore1@gsu.edu>
2024-10-31 01:00:21 -0400

    changed from tag$button style to actionButton, more succinct and potentially better suited for Shiny event handling

--- modules/paletteButtonUI.R
+++ modules/paletteButtonUI.R
@@ -1,26 +1,17 @@
 ## Module for palette buttons
+
 paletteButtonUI <- function(id) {
   ns <- NS(id)  # Create a namespace
   
-  div(style = "position: absolute; bottom: 20px; right: 20px; display: flex; flex-direction: column; align-items: flex-start;",
+  div(style = "position: absolute; bottom: 20px; right: 20px; 
+               display: flex; flex-direction: column; align-items: flex-start;",
+      
       # Title above the buttons
-      h5(style = "margin-left: 20px;","Change Box Color Scheme"),
+      h5(style = "margin-left: 15px;", "Change Box Color Scheme"),
       div(style = "display: flex;",
-          tags$button(
-            id = ns("c1_bttn"),
-            class = "btn action-button",
-            tags$img(src = "cividis.png", height = "50px")  # Path remains the same
-          ),
-          tags$button(
-            id = ns("c2_bttn"),
-            class = "btn action-button",
-            tags$img(src = "mako.png", height = "50px")     # Path remains the same
-          ),
-          tags$button(
-            id = ns("c3_bttn"),
-            class = "btn action-button",
-            tags$img(src = "rocket.png", height = "50px")   # Path remains the same
-          )
+          actionButton(inputId = ns("c1_bttn"), label = tags$img(src = "cividis.png", height = "45px")),
+          actionButton(inputId = ns("c2_bttn"), label = tags$img(src = "mako.png", height = "45px")),
+          actionButton(inputId = ns("c3_bttn"), label = tags$img(src = "rocket.png", height = "45px"))
       )
   )
 }
