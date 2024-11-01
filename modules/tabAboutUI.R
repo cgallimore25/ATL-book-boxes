9f596e40c7f5fe840f4aac4e7709796a39edaf8b
Connor Gallimore <cgallimore1@gsu.edu>
2024-10-31 22:40:04 -0400

    finishing touches

--- modules/tabAboutUI.R
+++ modules/tabAboutUI.R
@@ -6,7 +6,8 @@ tabAboutUI <- function(id) {
   fluidPage(
     
     h3("Inspiration"),
-    p("In 2023, I noticed that many of the public bookcases in my area were not listed on the Little Free Library web map.
+    p("This project combines my love of open-source data with resource sharing. 
+       In 2023, I noticed that many of the public bookcases in my area were not listed on the Little Free Library web map.
        I began recording longitude and latitude coordinates for the ones I discovered and cross-referencing them with the listings.
        The result is this dataset of 105 bookboxes in the Greater Atlanta area."),
     
@@ -16,13 +17,13 @@ tabAboutUI <- function(id) {
        tags$a(href = "https://www.diversitydatakids.org/child-opportunity-index?_ga=2.130754447.1489633750.1679850921-1316632825.1679594824", "Child Opportunity Index", target = "_blank"), "(COI).
        The COI is a composite index of children's neighborhood opportunity, consisting of 3 major domains: Education (ED), Health and Environment (HE), and Social and Economic factors (SE).
        See how coloring the zip areas and box points by different COI variables relate to the (far from exhaustive) distribution of bookboxes!
-       Read below for more details about the variables and functionalities."),
+       Read on for details about the variables and user interactions."),
     
     hr(),
     h4("Explanation of variables:"),
     tags$ul(
       tags$li(tags$code("Ed resources"), tags$code("Health, env, safety"), tags$code("Socioeconomic"), "and", tags$code("Composite COI"), "here represent the nationally normalized major domain and composite scores (out of 100).
-              Read more about them in the COI 3.0", tags$a(href = "https://data.diversitydatakids.org/dataset/coi30-2010-tracts-child-opportunity-index-3-0-database--2010-census-tracts/resource/0c292d45-8a97-494a-908a-3f937516da3a", "overall", target = "_blank"),  
+              See COI 3.0", tags$a(href = "https://data.diversitydatakids.org/dataset/coi30-2010-tracts-child-opportunity-index-3-0-database--2010-census-tracts/resource/0c292d45-8a97-494a-908a-3f937516da3a", "overall", target = "_blank"),  
               "and", tags$a(href= "https://data.diversitydatakids.org/dataset/coi30-2010-tracts-child-opportunity-index-3-0-database--2010-census-tracts/resource/8c7305d8-05f6-494b-bcc9-845b305258e1", "subdomain", target = "_blank"), "data dictionaries."),
       tags$li(tags$code("Community diversity"), "is an entropy calculation of the American Indian/Alaska Native, Asian/Pacific Islander, Black, White, Hispanic, and Other as proportions of that zip code's population (also contained in the COI dataset). 
               Higher values indicate a more uniform community composition; low values suggest one/few groups represent most people."),
