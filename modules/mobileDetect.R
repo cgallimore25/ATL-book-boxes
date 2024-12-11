19312f31fe8cfd20bed8934abad6fc85c7131955
Connor <45884644+cgallimore25@users.noreply.github.com>
2024-12-11 13:38:39 -0500

    Function that runs the mobile.js routine

---
+++ modules/mobileDetect.R
@@ -0,0 +1,11 @@
+## Run mobile.js routine
+# Assumes device type will not change within this session
+
+mobileDetect <- function(inputId, value = 0) {
+  tagList(
+    singleton(tags$head(tags$script(src = "js/mobile.js"))),
+    tags$input(id = inputId,
+               class = "mobile-element",
+               type = "hidden")
+  )
+}
