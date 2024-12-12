03ba72d6a3fa50a4c1a51ab0884a0fa51c87ff72
Connor Gallimore <45884644+cgallimore25@users.noreply.github.com>
2024-12-12 12:46:30 -0500

    Shift comments below taking up less space

--- ui.R
+++ ui.R
@@ -4,15 +4,19 @@ library(shinyWidgets)
 
 ui <- navbarPage("100+ ATL Book Boxes", id = "nav", collapsible = TRUE,
       
-      # Detect device type
-      # tags$head(device_detection_script),
-      # shinybrowser::detect(),
-      
       # Map UI module
-      # tabPanel("Interactive Map", tabMapUI("interactive_map", uiOutput("dynamic_ui"))), 
       tabPanel("Interactive Map", tabMapUI("interactive_map")),
       
       # About UI module
       tabPanel("About", tabAboutUI("about"))                     
 )
 
+
+
+## Mobile detect attempts
+# Detect device type
+# tags$head(device_detection_script),
+# shinybrowser::detect(),
+
+# Map UI module
+# tabPanel("Interactive Map", tabMapUI("interactive_map", uiOutput("dynamic_ui"))), 
