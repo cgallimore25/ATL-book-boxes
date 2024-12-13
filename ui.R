ddbc9f173e87b4f36ab0b07fcf32f525cd57f72c
Connor Gallimore <45884644+cgallimore25@users.noreply.github.com>
2024-12-13 02:27:36 -0500

    Removed temp comments

--- ui.R
+++ ui.R
@@ -4,24 +4,9 @@ library(shinyWidgets)
 
 ui <- navbarPage("100+ ATL Book Boxes", id = "nav", collapsible = TRUE,
       
-      # Include the mobile detection script
-      # tags$head(
-      #   tags$script(src = "mobile-detect.js")
-      # ),
-      
       # Map UI module
       tabPanel("Interactive Map", tabMapUI("interactive_map")),
       
       # About UI module
       tabPanel("About", tabAboutUI("about"))                     
 )
-
-
-
-## Mobile detect attempts
-# Detect device type
-# tags$head(device_detection_script),
-# shinybrowser::detect(),
-
-# Map UI module
-# tabPanel("Interactive Map", tabMapUI("interactive_map", uiOutput("dynamic_ui"))), 
