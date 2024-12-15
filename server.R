d617f2011a9c20ec171f89bd9deb57ae8359bb04
Connor Gallimore <45884644+cgallimore25@users.noreply.github.com>
2024-12-14 20:17:01 -0500

    Remove old comments

--- server.R
+++ server.R
@@ -4,24 +4,5 @@ server <- function(input, output, session) {
   
   # Call Map server module
   tabMapServer("interactive_map", n_bins, var_lookup, zip_df, sub_z_srt, merged_dat)
+  
 }
-
-
-
-## Mobile detect attempts
-# Cache the device type at run time
-# observeEvent(input$is_mobile, {
-#   session$userData$is_mobile <- input$is_mobile
-# }, once = TRUE)  
-
-# Dynamically generate the map UI based on device type
-# output$dynamic_ui <- renderUI({
-#   req(session$userData$is_mobile)  # Ensure `is_mobile` is available
-#   tabMapUI("interactive_map", is_mobile = session$userData$is_mobile)
-# })
-
-# print(shinybrowser::get_device())
-
-# output$dynamic_ui <- renderUI({
-#   tabMapUI("interactive_map", is_mobile = shinybrowser::get_device() == "Mobile")
-# })
