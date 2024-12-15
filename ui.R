c53928b5db4ce8a82dcad05f454270707559b609
Connor Gallimore <45884644+cgallimore25@users.noreply.github.com>
2024-12-14 20:15:34 -0500

    Set navbar z-index in using tags

--- ui.R
+++ ui.R
@@ -2,11 +2,20 @@
 
 library(shinyWidgets)
 
-ui <- navbarPage("100+ ATL Book Boxes", id = "nav", collapsible = TRUE,
-      
+ui <- navbarPage("100+ ATL Book Boxes", id = "nav", collapsible = TRUE, 
+                 
+      tags$head(
+        tags$style(HTML("
+          .navbar {
+            z-index: 1100; /* Set navbar on top */
+          }
+        "))
+      ),
+                            
       # Map UI module
       tabPanel("Interactive Map", tabMapUI("interactive_map")),
       
       # About UI module
-      tabPanel("About", tabAboutUI("about"))                     
+      tabPanel("About", tabAboutUI("about")) 
+      
 )
