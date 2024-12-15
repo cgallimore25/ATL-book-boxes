bad47d28bcb4afec6167e23e724854c62e6c991d
Connor Gallimore <45884644+cgallimore25@users.noreply.github.com>
2024-12-14 20:22:42 -0500

    Added conditional positioning/opacity to pal buttons

--- modules/paletteButtonUI.R
+++ modules/paletteButtonUI.R
@@ -1,18 +1,33 @@
 ## Module UI for palette buttons
 # create a row of 3 buttons from palette images
 
-paletteButtonUI <- function(id) {
+paletteButtonUI <- function(id, is_mobile) {
   ns <- NS(id)  # Create a namespace
   
-  div(style = "position: absolute; bottom: 20px; right: 20px; 
-               display: flex; flex-direction: column; align-items: flex-start;",
+  # Determine the flex-direction based on is_mobile
+  flex_direction <- if (is_mobile) "column" else "row"
+  position_side <- if (is_mobile) "left: 20px;" else "right: 20px;"
+  position_vert <- if (is_mobile) "bottom: 45px;" else "bottom: 20px;"
+  button_title <- if (is_mobile) "" else "Change Box Color Scheme"
+  opacity <- if (is_mobile) "opacity: 0.6;"  else "opacity: 0.6;"
+  height <- if (is_mobile) "30px"  else "45px"
+  
+  div(
+    style = glue::glue("
+      position: absolute; {position_vert} {position_side};
+      display: flex; flex-direction: column; align-items: flex-start;
+    "),
       
       # Title above the buttons
-      h5(style = "margin-left: 15px;", "Change Box Color Scheme"),
-      div(style = "display: flex;",
-          actionButton(inputId = ns("c1_bttn"), label = tags$img(src = "cividis.png", height = "45px")),
-          actionButton(inputId = ns("c2_bttn"), label = tags$img(src = "mako.png", height = "45px")),
-          actionButton(inputId = ns("c3_bttn"), label = tags$img(src = "rocket.png", height = "45px"))
+      h5(style = "margin-left: 15px;", button_title),
+      div(
+        class = "palette-buttons",
+        style = glue::glue("
+        {opacity} display: flex; flex-direction: {flex_direction};
+      "),  # Add spacing between buttons
+          actionButton(inputId = ns("c1_bttn"), label = tags$img(src = "cividis.png", height = height)),
+          actionButton(inputId = ns("c2_bttn"), label = tags$img(src = "mako.png", height = height)),
+          actionButton(inputId = ns("c3_bttn"), label = tags$img(src = "rocket.png", height = height))
       )
   )
 }
