55babbfe9d719d97a84f67ba6548c115db1f2b03
Connor Gallimore <45884644+cgallimore25@users.noreply.github.com>
2024-12-14 20:22:06 -0500

    Add tags control z-indices, separate uiOutputs for panels

--- modules/tabMapUI.R
+++ modules/tabMapUI.R
@@ -4,20 +4,20 @@ tabMapUI <- function(id) {
   ns <- NS(id)  # Create a namespace for the module
   
   fluidPage(
-    
+
     # Make height adaptable to screen, bring drop-downs forward
     tags$style(type = "text/css", glue::glue("
       #{ns('map')} {{height: calc(100vh - 80px) !important; z-index: 1000;}}
+      div[id$='control_panel'] {{ z-index: 1010; position: absolute; }} /* Adjust z-index for the panel */
       .dropdown-menu {{ z-index: 1050; }} /* Higher z-index for dropdowns */
-      .panel {{ z-index: 1010; }}         /* Adjust z-index for the panel */
+      .attribution-overlay {{ z-index: 1100; position: absolute; }} /* Ensure overlay is on top */
+      .palette-buttons {{ z-index: 1040; }}
     ")),
     
     mobileDetect('isMobile'),
     
     uiOutput(ns("control_panel")),
-    
-    # Color palette buttons module (positioned in bottom-right corner of UI)
-    paletteButtonUI(ns("palette_buttons")),
+    uiOutput(ns("palette_buttons")),
     
     # Leaflet map main panel
     mainPanel(
@@ -25,8 +25,9 @@ tabMapUI <- function(id) {
     ),
     
     # Attribution overlay at the bottom center
-    tags$div(style = "position: absolute; bottom: 7.5px; left: 35%; 
-             transform: translateX(-50%); color: gray; font-size: 12px;",
+    tags$div(class = "attribution-overlay",
+             style = "bottom: 7.5px; left: 30%; transform: translateX(-50%); 
+                      color: darkslategray; font-size: 12px;",
              "Data compiled by ", tags$em('Connor Gallimore'))
   )
 }
