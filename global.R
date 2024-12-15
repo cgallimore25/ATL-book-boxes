5f12648f668cf4d16b60afc2c98f9255a82b05d4
Connor Gallimore <45884644+cgallimore25@users.noreply.github.com>
2024-12-15 03:09:53 -0500

    Remove old inline js routine

--- global.R
+++ global.R
@@ -98,14 +98,6 @@ n_boxes <- nrow(box_data)
 var_lookup <- setNames(as.list(names(box_choices)), unlist(box_choices))
 
 
-# Determine device type with JavaScript
-# device_detection_script <- tags$script("
-#   $(document).on('shiny:connected', function() {
-#     var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-#     Shiny.setInputValue('is_mobile', isMobile);
-#   });
-# ")
-
 
 # Load modules
 source("modules/controlPanelUI.R")
